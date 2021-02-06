6e97d30fe93dad04a8648ba91e299943b069ea9e
Anantjit <anantjitchander@gmail.com>
2021-02-06 17:03:40 +0530

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -9,7 +9,6 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
 
 
 
@@ -18,4 +17,30 @@ var firebaseConfig = {
 function user(){
     user_name=localStorage.getItem("Username");
     firebase.database().ref("/").child(user_name).update({purpose:"adding user"});
+}
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names = childKey;
+ console.log("room name-"+Room_names);
+ row="<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div> <hr> ";
+ document.getElementById("output").innerHTML+=row;
+ 
+  //Start code
+
+ //End code
+ });});}
+getData();
+
+
+function redirectToRoomName(name){
+  localStorage.setItem("room_name", name);
+  window.location="chat_room.html";
+}
+
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+
+  window.location="index.html";
 }
